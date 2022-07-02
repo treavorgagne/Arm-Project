8c64ba460a137bca9d8e9988921e0c02d06116f3
Treavor Gagne <gagne.16@hotmail.com>
2022-07-02 12:26:10 -0700

    RGB to YCC added

--- arm.c
+++ arm.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <unistd.h>
-
-uint8_t* convertRGBtoYCC(uint8_t R, uint8_t G, uint8_t B){
-    uint8_t* YCC = (uint8_t*) malloc(3 * sizeof(uint8_t));
-    
-    uint8_t y  = (uint8_t)( 0.299   * R + 0.587   * G + 0.114   * B);
-    uint8_t cb = (uint8_t)(-0.16874 * R - 0.33126 * G + 0.50000 * B);
-    uint8_t cr = (uint8_t)( 0.50000 * R - 0.41869 * G - 0.08131 * B);
-
-    YCC[0] = y;
-    YCC[1] = cb;
-    YCC[2] = cr;
-
-    return YCC;
-}
+// #include <unistd.h>
 
 typedef struct rgb_pixel {
     uint8_t red;
@@ -23,10 +9,36 @@ typedef struct rgb_pixel {
     uint8_t blue;
 } rgb_pixel;
 
-typedef struct image_data { 
+typedef struct ycc_pixel {
+    float y;
+    float cb;
+    float cr;
+} ycc_pixel;
+
+typedef struct ycc_image {
+    ycc_pixel *data;
+} ycc_image;
+
+typedef struct rgb_image { 
     rgb_pixel *data;
-} image_data;
+} rgb_image;
  
+ycc_pixel convertRGBtoYCC(uint8_t R, uint8_t G, uint8_t B){
+    ycc_pixel YCC;
+
+    float y  = (float)(16 + (0.257 * R) +( 0.587 * G) + (0.098 * B));
+    float cb = (float)(128 + (-0.148 * R) - (0.291 * G) + (0.439 * B));
+    float cr = (float)(128 + (0.439 * R) - (0.368 * G) - (0.071 * B));
+
+    YCC.y = y;
+    YCC.cb = cb;
+    YCC.cr = cr;
+
+    return YCC;
+}
+
+
+
 int main()
 {
     // open file in binary read mode
@@ -37,7 +49,9 @@ int main()
    }
 
     // skip header of jpg file
-    fseek( fptr, 54, SEEK_SET );
+    int header_size = 54;
+    // fread(, header_size, header_size, fptr);
+    fseek( fptr, header_size, SEEK_SET );
     // TODO get width and heigth from header
     
     int width = 400;
@@ -45,18 +59,22 @@ int main()
 
     // initialize data size 
     int image_size = width*height;
-    image_data *input_data;
-    input_data = malloc(sizeof(image_data));
+    rgb_image *input_data;
+    input_data = malloc(sizeof(rgb_image));
     input_data->data = malloc(sizeof(rgb_pixel)*image_size);
 
+    ycc_image *output_data;
+    output_data = malloc(sizeof(ycc_image));
+    output_data->data = malloc(sizeof(ycc_pixel)*image_size);
+    
     // read all rgb data into input_data
     fread(input_data->data, sizeof(rgb_pixel), image_size, fptr);
     
     for (int i = 0; i < width; i++){
         for (int j = 0; j < height; j++){
             int offset = i*width;
-            convertRGBtoYCC(input_data->data[offset+j].red, input_data->data[offset+j].green, input_data->data[offset+j].blue)
-            // printf("[%d][%d] RGB: %d %d %d\n",i,j, input_data->data[offset+j].red, input_data->data[offset+j].green, input_data->data[offset+j].blue);
+            output_data->data[offset+j] = convertRGBtoYCC(input_data->data[offset+j].red,input_data->data[offset+j].green,input_data->data[offset+j].blue);
+            printf("[%d][%d] YCC: %f %f %f\n",i,j, output_data->data[offset+j].y, output_data->data[offset+j].cb, output_data->data[offset+j].cr);
         }
     }
 
